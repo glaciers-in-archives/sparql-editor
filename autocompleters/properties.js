a7025af018a55a60a292b531e53565b084aeba00
Abbe98 <albin.post@gmail.com>
2021-04-11 22:15:51 +0200

    group properties by type
    (without duplicates)
    
    side effect added missing ones and depricated some

--- autocompleters/properties.js
+++ autocompleters/properties.js
@@ -1,32 +1,39 @@
 const properties = [
-  'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+  // landform
   'https://schema.org/name',
+  'https://schema.org/latitude',
   'https://schema.org/longitude',
   'https://schema.org/description',
-  'https://schema.org/latitude',
-  'https://www.glaciersinarchives.org/terms#type',
-  'http://www.w3.org/2004/02/skos/core#prefLabel',
-  'http://www.w3.org/2004/02/skos/core#definition',
-  'http://www.w3.org/2000/01/rdf-schema#seeAlso',
-  'http://www.w3.org/2004/02/skos/core#narrower',
-  'http://www.w3.org/2004/02/skos/core#closeMatch',
-  'http://www.w3.org/2004/02/skos/core#broader',
+  'https://schema.org/sameAs',
+  'https://schema.org/relatedLink',
+  'https://schema.org/containedInPlace',
+  'https://schema.org/containsPlace',
+
+  // record
   'https://schema.org/image',
   'https://schema.org/provider',
-  'https://schema.org/sameAs',
   'https://schema.org/isBasedOn',
   'https://schema.org/temporial',
   'https://schema.org/license',
-  'https://schema.org/relatedLink',
   'https://schema.org/additionalType',
   'https://schema.org/creator',
   'https://schema.org/spatial',
+
+  // annotations
   'http://www.w3.org/ns/oa#hasTarget',
   'http://www.w3.org/ns/oa#hasBody',
   'http://www.w3.org/ns/oa#motivatedBy',
   'http://www.w3.org/ns/prov#wasDerivedFrom',
   'http://purl.org/dc/terms/issued',
   'http://purl.org/dc/terms/creator',
+
+  // skos data
+  'http://www.w3.org/2004/02/skos/core#prefLabel',
+  'http://www.w3.org/2004/02/skos/core#definition',
+  'http://www.w3.org/2000/01/rdf-schema#seeAlso',
+  'http://www.w3.org/2004/02/skos/core#narrower',
+  'http://www.w3.org/2004/02/skos/core#closeMatch',
+  'http://www.w3.org/2004/02/skos/core#broader',
 ];
 
 const customPropertyCompleter = function(yasqe) {
